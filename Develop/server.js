99b9811144337a7f01c7e89416a40ee68f5dc3a2
unknown <Jgossett73@gmail.com>
2022-08-18 06:59:32 -0400

    push

--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,55 @@
+const fs = require("fs");
 const express = require("express");
-const path = require('path');
-const fs = require('fs');
-const dbData = require('./db/db.json');
+const path = require("path");
 
 const app = express();
-const port = 3001;
+// Sets port for listening and let heroku decide on port, if not, use port 3005
+const PORT = process.env.PORT || 3001;
 
+//serve images, CSS files, and JavaScript files in a directory named public
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/notes' , function (req, res) {
-    res.sendFile(path.join(mainDir,"notes.html"));
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get('/api/notes', function(req, res){
-    res.sendFile(path.join(__dirname, dbData));
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
 });
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let notelength = (noteList.length).toString();
+
+    
+    newNote.id = notelength;
+
+    noteList.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+    res.json(noteList);
+})
+
+//delete note according to their tagged id.
+app.delete("/api/notes/:id", (req, res) => {
+    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteId = (req.params.id).toString();
+
+
+    noteList = noteList.filter(selected =>{
+        return selected.id != noteId;
+    })
+
+   fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+    res.json(noteList);
+});
+
+
+app.listen(PORT, () => console.log("Server listening on port " + PORT));
