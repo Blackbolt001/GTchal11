14b1c09ecd8461ab2d09efda4f3705f078b9155f
unknown <Jgossett73@gmail.com>
2022-08-18 06:47:41 -0400

    starting server

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require('path');
+const fs = require('fs');
+const dbData = require('./db/db.json');
+
+const app = express();
+const port = 3001;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.get('/notes' , function (req, res) {
+    res.sendFile(path.join(mainDir,"notes.html"));
+});
+
+app.get('/api/notes', function(req, res){
+    res.sendFile(path.join(__dirname, dbData));
+});
