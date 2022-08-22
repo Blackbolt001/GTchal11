bd0d2fda8fcb2193f301d44b117a34816d09e296
unknown <Jgossett73@gmail.com>
2022-08-22 15:59:03 -0400

    server issues

--- server.js
+++ server.js
@@ -9,51 +9,19 @@ const PORT = process.env.PORT || 3001;
 
 
 const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 //serve images, CSS files, and JavaScript files in a directory named public
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(express.json());
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+app.use('/api',apiRoutes);
+app.use('/',htmlRoutes);
+
+
+
+
+app.listen(PORT, () => {
+    console.log("Server listening on port " + 'PORT || 3001');
 });
-
-app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
-});
-
-app.get("'/'", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-app.post("/api/notes", (req, res) => {
-    let newNote = req.body;
-    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let notelength = (noteList.length).toString();
-
-    
-    newNote.id = notelength;
-
-    noteList.push(newNote);
-
-    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
-    res.json(noteList);
-})
-
-//delete note according to their tagged id.
-app.delete("/api/notes/:id", (req, res) => {
-    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let noteId = (req.params.id).toString();
-
-
-    noteList = noteList.filter(selected =>{
-        return selected.id != noteId;
-    })
-
-   fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
-    res.json(noteList);
-});
-
-
-app.listen(PORT, () => console.log("Server listening on port " + '${PORT}'));
